12a33c794ec496ce15dd8506ee8cfea5ab475bd2
thinny <tony.sutin1234@gmail.com>
2018-01-28 21:26:27 +0700

    add ionic3 flashlight support cordova 6.3.x
    add ionic3 flashlight support cordova 6.3.x

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,28 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { Flashlight } from '@ionic-native/flashlight';
 
 @Component({
   selector: 'page-home',
-  templateUrl: 'home.html'
+  templateUrl: 'home.html',
+  providers: [Flashlight]
 })
 export class HomePage {
-
-  constructor(public navCtrl: NavController) {
-
+  btn_title: any;
+  constructor(public navCtrl: NavController, private fl: Flashlight) {
+    this.btn_title = 'turn on Flashlight';
+  }
+  switchFlashLight(){
+    this.fl.available().then((resp) => {
+      this.fl.toggle().then(data => {
+        console.log(data)
+      }).catch(err => {
+        console.log(err)
+      })
+    }).catch(err => {
+      console.log(err)
+      alert('FlashLight is not available');
+    });
   }
 
 }
